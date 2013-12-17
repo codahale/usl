75d614d4da914946fc3d94d6228b3347ff889e9b
Coda Hale <coda.hale@gmail.com>
2013-12-17 09:54:50 -0800

    Added tests for sorting.

--- model_test.go
+++ model_test.go
@@ -1,9 +1,35 @@
 package usl
 
 import (
+	"fmt"
 	"math"
+	"sort"
 	"testing"
 )
+
+func TestMeasurementSorting(t *testing.T) {
+	measurements := MeasurementSet{
+		Measurement{X: 72, Y: 1853},
+		Measurement{X: 108, Y: 1829},
+		Measurement{X: 18, Y: 996},
+		Measurement{X: 216, Y: 1702},
+		Measurement{X: 36, Y: 1652},
+		Measurement{X: 144, Y: 1775},
+		Measurement{X: 1, Y: 65},
+	}
+
+	sort.Sort(measurements)
+
+	expected := "1,18,36,72,108,144,216,"
+	actual := ""
+	for _, m := range measurements {
+		actual += fmt.Sprintf("%v,", m.X)
+	}
+
+	if actual != expected {
+		t.Fatalf("Expected %v but was %v", expected, actual)
+	}
+}
 
 func TestBuild(t *testing.T) {
 	measurements := MeasurementSet{
